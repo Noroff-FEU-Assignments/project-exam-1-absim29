f40272289696f0590f9c127ee2f7ba77c4849235
absim29 <simon.bianca29@gmail.com>
2023-09-18 12:50:01 +0200

    load more

--- javascript/allposts.js
+++ javascript/allposts.js
@@ -4,40 +4,55 @@ import { getPosts } from "./index.js";
 async function renderList() {
     const list = await getPosts();
     const listAll = document.querySelector(".posts");
+    const posts = list.slice(0,10);
+    const extra = list.slice(10, list.length);
 
-        listAll.innerHTML = ``;
-        list.forEach(post => {
-            listAll.innerHTML += 
-                `<a href="../singlepost.html?id=${post.id}">
+    listAll.innerHTML = ``;
+
+    for(let i = 0; i < posts.length; i++) {
+        
+        listAll.innerHTML += 
+                `<a href="../singlepost.html?id=${posts[i].id}">
                     <div class="post-wrapper">
-                        <img src="${post._embedded["wp:featuredmedia"][0].source_url}" alt="Image of dessert" id="postimg">
+                        <img src="${posts[i]._embedded["wp:featuredmedia"][0].source_url}" alt="Image of dessert" id="postimg">
                         <div id="post-text">
-                            <h2>${post.title.rendered}</h2>
-                            <p>${post.excerpt.rendered}</p>
+                            <h2>${posts[i].title.rendered}</h2>
+                            <p>${posts[i].excerpt.rendered}</p>
                         </div>
                     </div>
                 </a>`;
-
-        });
-
+                
     }
+
+
+    const loadMoreButton = document.querySelector('#load-more');
+    const totalCount = list.length;
+
+    loadMoreButton.addEventListener('click', () => {
+        for(let i = 0; i < extra.length; i++) {
+            
+        if(posts.length < totalCount) {
+            listAll.innerHTML += 
+                    `<a href="../singlepost.html?id=${extra[i].id}">
+                        <div class="post-wrapper">
+                            <img src="${extra[i]._embedded["wp:featuredmedia"][0].source_url}" alt="Image of dessert" id="postimg">
+                            <div id="post-text">
+                                <h2>${extra[i].title.rendered}</h2>
+                                <p>${extra[i].excerpt.rendered}</p>
+                            </div>
+                        </div>
+                    </a>`;
+            // listAll.append();
+
+        }
+        if(totalCount){
+            loadMoreButton.style.display = "none";
+        }
+
+    }});
+
+}
 renderList();
 
 
-// const loadMoreButton = document.querySelector('#load-more');
-// const posts = list.slice(0,10);
-// const chunkSize = 10;
 
-
-// loadMoreButton.addEventListener('click', () => {
-//     renderList();
-
-//     for(let i = 0; i < posts.length; i+= chunkSize) {
-//     const chunk = posts.slice(i, i + chunkSize);
-//     console.log(chunk)
-
-//     }
-
-// posts.appendChild(posts);
-
-// })
